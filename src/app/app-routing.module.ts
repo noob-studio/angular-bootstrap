5a651ed843450f14e237829b178447ea70d49a88
Pathomporn Muangkham <m.pathomporn@geeksterdigital.com>
2020-02-22 18:46:50 +0700

    ep01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { AboutComponent } from './page/about/about.component';
+import { FeaturesComponent } from './page/features/features.component';
+import { HomeComponent } from './page/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'home',
+  pathMatch: 'full',
+},{
+  path: 'home',
+  component: HomeComponent,
+},{
+  path: 'features',
+  component: FeaturesComponent,
+},{
+  path: 'about',
+  component: AboutComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AboutComponent } from './page/about/about.component';
+import { FeaturesComponent } from './page/features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
